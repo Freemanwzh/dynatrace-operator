4f3f43be08cdb1e5d6139efa45571a520bf9a2ef
adam.orcholski <adam.orcholski@dynatrace.com>
2021-05-24 18:29:13 +0200

    containerVolumeMounts and Volumes added to Capabilities (cleanup)

--- controllers/activegate/statefulset/statefulset.go
+++ controllers/activegate/statefulset/statefulset.go
@@ -291,10 +291,6 @@ func isProxyNilOrEmpty(proxy *dynatracev1alpha1.DynaKubeProxy) bool {
 	return proxy == nil || (proxy.Value == "" && proxy.ValueFrom == "")
 }
 
-func isKubernetesMonitoringEnabled(stsProperties *statefulSetProperties) bool {
-	return stsProperties != nil && stsProperties.DynaKube != nil && stsProperties.DynaKube.Spec.KubernetesMonitoringSpec.Enabled
-}
-
 func generateStatefulSetHash(sts *appsv1.StatefulSet) (string, error) {
 	data, err := json.Marshal(sts)
 	if err != nil {
